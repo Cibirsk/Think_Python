77fafe3529ea518f66ffc6fe6dc918147f148103
Cibirsk <stvl@gmx.com>
2021-05-05 11:14:19 +0200

    Update polygon.py

--- polygon.py
+++ polygon.py
@@ -1,14 +1,3 @@
-"""This module contains a code example related to
-
-Think Python, 2nd Edition
-by Allen Downey
-http://thinkpython2.com
-
-Copyright 2015 Allen Downey
-
-License: http://creativecommons.org/licenses/by/4.0/
-"""
-
 from __future__ import print_function, division
 
 import math
@@ -81,16 +70,16 @@ def circle(t, r):
 # running as a script, in which case run the test code,
 # or being imported, in which case don't.
 
-if __name__ == '__main__':
-    bob = turtle.Turtle()
+#if __name__ == '__main__':
+bob = turtle.Turtle()
 
-    # draw a circle centered on the origin
-    radius = 100
-    bob.pu()
-    bob.fd(radius)
-    bob.lt(90)
-    bob.pd()
-    circle(bob, radius)
+# draw a circle centered on the origin
+radius = 100
+bob.pu()
+bob.fd(radius)
+bob.lt(90)
+bob.pd()
+circle(bob, radius)
 
-    # wait for the user to close the window
-    turtle.mainloop()
+# wait for the user to close the window
+turtle.mainloop()
