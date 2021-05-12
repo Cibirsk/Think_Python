a1daf0ced58617d501c0683a618ef42c024d61da
Cibirsk <stvl@gmx.com>
2021-05-12 16:18:01 +0200

    Update test.py

--- test.py
+++ test.py
@@ -42,4 +42,21 @@ def myFibo(n):
 	else:
 		return myFibo(n-1)+ myFibo(n-2)
 
-print(myFibo(6))
+#print(myFibo(6))
+
+def hanoi(n, source, helper, target):
+    if n > 0:
+        # move tower of size n - 1 to helper:
+        hanoi(n - 1, source, target, helper)
+        # move disk from source peg to target peg
+        if source:
+            target.append(source.pop())
+        # move tower of size n-1 from helper to target
+        hanoi(n - 1, helper, source, target)
+        
+source = [4,3,2,1]
+target = []
+helper = []
+hanoi(len(source),source,helper,target)
+
+print(source, helper, target)
