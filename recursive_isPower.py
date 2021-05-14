96a7f389dff9ff693bee8ec2f53bbc026f43865a
Cibirsk <stvl@gmx.com>
2021-05-14 08:25:04 +0200

    Update recursive_isPower.py

--- recursive_isPower.py
+++ recursive_isPower.py
@@ -4,14 +4,16 @@ Write a function called is_power that takes parameters a and b
 and returns True if a is a power of b. 
 Note: you will have to think about the base case.
 """
+#code faux, la récursive ne fonctionne pas
 def isPower(a,b):
     if a%b == 0:
-        return True
+        if isPower(a/b, b):
+            return True
     
-    else:
-        return False
-    isPower(a/b, b)
+        else:
+            return False
 
+#code correct avec recursive
 def is_power(a, b):
     """Checks if a is power of b."""
     if a == b:
@@ -21,5 +23,7 @@ def is_power(a, b):
     else:
         return False
 
-print (isPower(6,2))
-print(is_power(6,2))
+a=4
+b=2
+print (isPower(a,b))
+print(is_power(a,b))
