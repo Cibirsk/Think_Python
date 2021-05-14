24af55d05eea9baba8d1ab873bae5764bb3ca8ef
Cibirsk <stvl@gmx.com>
2021-05-14 09:14:15 +0200

    Update recursive_GCD.py

--- recursive_GCD.py
+++ recursive_GCD.py
@@ -4,3 +4,16 @@ One way to find the GCD of two numbers is based on the observation that
 if r is the remainder when a is divided by b, then gcd(a, b) = gcd(b, r). 
 As a base case, we can use gcd(a, 0) = a.
 """
+
+def gcd(a,b):
+    if(b == 0):
+        return a 
+    else:
+        return gcd(b,a%b)
+
+print(gcd(561,357))
+
+# gcd(,b) = gcd(b,r)
+# gcd(a,O) = a
+
+
