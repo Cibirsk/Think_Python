b4cdbf4624a02446aa38c24acd432cb30006dcd8
Cibirsk <stvl@gmx.com>
2021-05-20 12:17:39 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,27 @@
 def iteReverse(string):
     result=''
     longueur= len(string)-1
-    while longueur > 0:
-        result= string(longueur)
+    while longueur >= 0:
+        result= result + string[longueur]
         longueur -= 1
     return result
 
-print(iteReverse('tap'))
+a='tap'
+#print(iteReverse(a))
+
+def recuReverse(string):
+    if len(string) == 0:
+        return string[longueur]
+    else:
+        return recuReverse(string[longueur-1])
+        
+def reverseString(string):
+    # Base Case
+    if len(string) == 0:
+        return string
+
+    # Recursive Case
+    else:
+        return reverseString(string[1:]) + string[0]
+
+print(reverseString('tapir'))
