cc50be0e5dee8e738356230f4e36b4013509e4c8
Cibirsk <stvl@gmx.com>
2021-05-30 17:25:46 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,32 @@
-def fibo(n):
-    if n== 0:
-        return 0
-    elif n==1:
-        return 1
-    else:
-        return fibo(n-1) + fibo(n-2)
+import math
 
-print(fibo(8))
+def factorial(n):
+    if n == 0:
+        return 1
+    return factorial(n-1)*n
+
+def estimate_pi():
+    k=0
+    total=0
+    factor= (2* math.sqrt(2))/9801
+    
+    while True:
+        num= factorial(4*k) * (1103+ (26390 * k))
+        den= (factorial(k)**4) * (396**(4*k))
+        temp= factor * num/den
+        total += temp
+        k +=1
+        if temp < 1e-15:
+            break 
+    
+    return 1/total
+
+print(estimate_pi())
+
+
+
+
+
+
+
+
