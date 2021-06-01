14d7b98635e34a0581c28a97d1e08d8eeec54f66
Cibirsk <stvl@gmx.com>
2021-06-01 16:48:18 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,32 +1,6 @@
-import math
+def both(word1,word2):
+    for letter in word1:
+        if letter in word2:
+            print(letter)
 
-def factorial(n):
-    if n == 0:
-        return 1
-    return factorial(n-1)*n
-
-def estimate_pi():
-    k=0
-    total=0
-    factor= (2* math.sqrt(2))/9801
-    
-    while True:
-        num= factorial(4*k) * (1103+ (26390 * k))
-        den= (factorial(k)**4) * (396**(4*k))
-        temp= factor * num/den
-        total += temp
-        k +=1
-        if temp < 1e-15:
-            break 
-    
-    return 1/total
-
-print(estimate_pi())
-
-
-
-
-
-
-
-
+both('abcdef', 'cf')
