61d383d0ef909198c17e10335b39420c5cdc3fdd
Cibirsk <stvl@gmx.com>
2021-06-06 18:34:04 +0200

    Update letters_to_avoid.py

--- letters_to_avoid.py
+++ letters_to_avoid.py
@@ -23,15 +23,24 @@ def useOnly(word, lettersList):
 
 #returns True if the word uses all the required letters at least once
 def use_all(letters_list):
-    fin = open('words2.txt') #open the file
+    fin = open('words.txt') #open the file
     countWord=0
+    tempCount=False
     for line in fin:
         for letter in letters_list:
             if letter in line:
-                countWord +=1
+                tempCount=True
             else:
-                countWord -=1
+                tempCount=False
+                break #permet d'arrêter la recherche, sinon incremente même si faux avant
+        if tempCount == True:
+            countWord +=1
     return countWord
 
-print(use_all('ir'))
+#returns True if the letters in a word appear in alphabetical order (double letters are ok)
+def is_abecedarian(word):
+    temp_word= word[1:]
+    for letter in word:
+        if letter < 
 
+print(is_abecedarian('bgr'))
