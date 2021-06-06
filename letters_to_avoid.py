b7aad096cf060596e67639af73fdea6135f37f67
Cibirsk <stvl@gmx.com>
2021-06-06 11:33:45 +0200

    Update letters_to_avoid.py

--- letters_to_avoid.py
+++ letters_to_avoid.py
@@ -1,15 +1,37 @@
 #find words without forbidden letters in a file
-fin = open('words2.txt')
-
-#    for line in fin:
-
 def avoid():
+    fin = open('words.txt') #open the file
     lettersOvoid = input(' Letters to avoid: ')
     countWord=0
     for line in fin:
         countWord +=1
-        if lettersOvoid in line:
-            countWord -=1
+        for letter in lettersOvoid:
+            if letter in line:
+                countWord -=1
+                break
     return countWord
 
-print(avoid())
+#returns True if the word contains only letters in the list
+def useOnly(word, lettersList):
+    response=False
+    for letter in word:
+        if letter in lettersList:
+            response=True
+        else:
+            response=False
+    return response
+
+#returns True if the word uses all the required letters at least once
+def use_all(letters_list):
+    fin = open('words2.txt') #open the file
+    countWord=0
+    for line in fin:
+        for letter in letters_list:
+            if letter in line:
+                countWord +=1
+                break
+                
+    return countWord
+
+print(use_all('ir'))
+
