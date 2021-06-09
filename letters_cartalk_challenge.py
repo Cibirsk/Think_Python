2383b2ef1a78dfb5f67a37f6f8a7fbd33c7bc6b6
Cibirsk <stvl@gmx.com>
2021-06-09 10:48:49 +0200

    none

--- letters_cartalk_challenge.py
+++ letters_cartalk_challenge.py
@@ -1,2 +1,18 @@
-fin = open('words2.txt') #open the file
 
+def readFile():
+    fin = open('words2.txt') #open the file
+    for word in fin:
+        cartalk1(word)
+
+def cartalk1(word):
+        i=0
+        theWord= word
+        while i < len(theWord)-1:
+            if word[i]==theWord[i+1] and theWord[i+2]==theWord[i+3] and theWord[i+4]==theWord[i+5]:
+                print(theWord)
+            else:
+                theWord = theWord[i+1:]
+                print(theWord)
+            i +=1
+
+readFile()
