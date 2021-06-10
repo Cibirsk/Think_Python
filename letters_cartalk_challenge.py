cb3dcc1ac72baa73549e240300e48279759dd9d8
Cibirsk <stvl@gmx.com>
2021-06-10 11:45:24 +0200

    none

--- letters_cartalk_challenge.py
+++ letters_cartalk_challenge.py
@@ -1,27 +1,29 @@
-
-def readFile():
+#find  words with three consecutive double letters
+def cartalk1():
     fin = open('words2.txt') #open the file
     for word in fin:
-        cartalk1(word)
-
-"""
-def cartalk1(word):
         i=0
-        theWord= word
-        while i < len(theWord)-1:
-            if word[i]==theWord[i+1] and theWord[i+2]==theWord[i+3] and theWord[i+4]==theWord[i+5]:
-                print(theWord)
-            else:
-                theWord = theWord[i+1:]
+        while i < len(word)-1:
+            if word[i]==word[i+1] and word[i+2]==word[i+3] and word[i+4]==word[i+5]:
+                print(word)
+                break
             i +=1
-"""
-def cartalk1(word):
-    theWord =word
-    for i in theWord:
-        if i==i+1 and i+2==i+3 and i+4==i+5:
-            print(theWord)
-        else:
-            #theWord = theWord[i+1:]
-            break
 
-readFile()
+#find palindromic numbers in 6 numbers suite
+# 1)      4 last palindromic, find this number
+# 2)  +1  5 last palindromic
+# 3)  +1  4 au the middle of 6 are palindromic
+# 4)  +1  6 palindromic 
+
+#def cartalk2():
+
+def isPalindromic(word):
+    response=False
+    for i in range(len(word)):
+        if word[i] == word[len(word)-1-i]:
+            response=True
+        else:
+            response=False
+    return response
+
+print(isPalindromic('azerta'))
