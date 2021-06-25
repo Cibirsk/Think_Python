64bc0b5faef549b7f8b2a404419dce676e1acaf8
Cibirsk <stvl@gmx.com>
2021-06-25 10:42:49 +0200

    none

--- list.py
+++ list.py
@@ -150,5 +150,28 @@ def wordList():
             #theList=theList + [i]   traitement beaucoup plus long
     print(theList)
 
+"""
+To check whether a word is in the word list, you could use the in operator, but it
+would be slow because it searches through the words in order.
+Because the words are in alphabetical order, we can speed things up with a bisection search (also
+known as binary search), which is similar to what you do when you look a word up in the dictionary
+(the book, not the data structure). You start in the middle and check to see whether the word you are
+looking for comes before the word in the middle of the list. If so, you search the first half of the list
+the same way. Otherwise you search the second half.
+Either way, you cut the remaining search space in half. If the word list has 113,809 words, it will
+take about 17 steps to find the word or conclude that it’s not there.
+Write a function called in_bisect that takes a sorted list and a target value and returns True if
+the word is in the list and False if it’s not.
+Or you could read the documentation of the bisect module and use that! Solution: http: //
+thinkpython2. com/ code/ inlist. py .
+"""
+def makeList():
+    theList=[]
+    fin = open('words2.txt')
+    for word in fin:
+        theList.append(word)
+    return theList
 
+#def bisectList():
+print(makeList())
 
